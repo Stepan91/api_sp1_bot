006c014610245c968ebfc7d74b6d83b1ac453a77
Stepan Pryn <prynstepan@yandex.ru>
2020-11-06 21:39:59 +0300

    1st try hw telegram bot

--- homework.py
+++ homework.py
@@ -6,15 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    current_timestamp = int(time.time())
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    if homework_status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,14 +24,15 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    params = {'from_date': current_timestamp}
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    homework_statuses = requests.get(url, headers=headers, params=params)
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
@@ -50,4 +53,5 @@ def main():
 
 
 if __name__ == '__main__':
+    bot.polling(none_stop=True)
     main()
