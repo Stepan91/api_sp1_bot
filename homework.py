a0d5e5e34ab06dfa1e0ffac5aa3192bd82854014
Stepan Pryn <prynstepan@yandex.ru>
2020-11-07 16:27:53 +0300

    вторая итерация после правок

--- homework.py
+++ homework.py
@@ -8,46 +8,48 @@ import logging
 load_dotenv()
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+VALID_VALUES = ('rejected', 'approved',)
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    try:
+    while True:
         homework_name = homework['homework_name']
         homework_status = homework['status']
-    except KeyError as wrong_key: 
-        logging.error(wrong_key, exc_info=True)
-    except IndexError as index_err:
-        logging.error(index_err, exc_info=True)
-    if homework_status != "rejected" or homework_status != "approved":
-        logging.error(msg=f"Статус может быть только <approved>"
-                          f"или <rejected>", exc_info=True)
-    if homework_status == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        if homework_name == None:
+            logging.error(msg='Ошибка в получении имени ДЗ', exc_info=True)
+        if homework_status == None:
+            logging.error(msg='Ошибка в получении статуса ДЗ', exc_info=True)
+        if homework_status not in VALID_VALUES:
+            logging.error(msg='Статус может быть только <approved>\
+                           или <rejected>', exc_info=True)
+            time.sleep(5)
+            continue
+        if homework_status == 'rejected':
+            verdict = 'К сожалению в работе нашлись ошибки.'
+        else:
+            verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
-# в каких случаях current_timestamp=None,
-# если в main оно всегда равно int(time.time())?
 def get_homework_statuses(current_timestamp=None):
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     params = {'from_date': current_timestamp}
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    try:
-        homework_statuses = requests.get(url, headers=headers, params=params)
-        return homework_statuses.json()
-    except (requests.RequestException, ValueError):
-        logging.error(msg = 'index_err', exc_info=True)
+    while True:
+        try:
+            homework_statuses = requests.get(url, headers=headers, params=params)
+            return homework_statuses.json()
+        except (requests.RequestException, ValueError):
+            logging.error(msg = 'index_err', exc_info=True)
+            time.sleep(5)
+            continue
 
 
 def send_message(message):
